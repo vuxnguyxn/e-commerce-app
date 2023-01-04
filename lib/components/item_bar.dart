3368ca51ad1326cb0e0207246736b0f95f1569f7
Trường Vũ <truongvu6021@gmail.com>
2023-01-04 21:09:34 +0700

    feat: custom bottom navigation item bar

---
+++ lib/components/item_bar.dart
@@ -0,0 +1,44 @@
+import 'package:e_commerce_app/constants.dart';
+import 'package:e_commerce_app/size_config.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class ItemBar extends StatelessWidget {
+  const ItemBar({
+    Key? key,
+    required this.press,
+    required this.icon,
+    required this.title,
+    this.isSelected = false,
+  }) : super(key: key);
+
+  final VoidCallback press;
+  final IconData icon;
+  final String title;
+  final bool isSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: press,
+      borderRadius: BorderRadius.circular(10),
+      child: Container(
+        padding: const EdgeInsets.symmetric(
+            horizontal: kDefaultPadding * .5, vertical: kDefaultPadding * .5),
+        child: Column(
+          children: [
+            Icon(icon),
+            SizedBox(
+              height: getProportionateScreenHeight(10),
+            ),
+            Text(
+              title,
+              style: TextStyle(
+                  color: isSelected ? Colors.black : Colors.black38),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
